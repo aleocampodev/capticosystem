391e46d1b4e05eee83ca52ce1d02a314031ca458
Omaira Alejandra Ocampo Tapias <omaira.527@hotmail.com>
2019-09-11 19:58:34 -0500

    Se modifica slider

--- js/index.js
+++ js/index.js
@@ -3,6 +3,42 @@ require('../sass/style.scss');
 require('./particles');
 require('./app');
 
+var movie = document.getElementById("container");
+var buttonTop = document.getElementById("button-top");
+var buttonBottom = document.getElementById("button-bottom");
+var wrapper = movie.querySelector(".wrapper");
+var sliderbox = movie.querySelectorAll(".wrappers");
+console.log(sliderbox)
+
+//Punto de inicio
+var start = 0;
+var sliderBottom = 0;
+var lengths = sliderbox.length;
+console.log(lengths);
+var boxHeight = movie.clientHeight;
+console.log(boxHeight);
+
+
+wrapper.style.height = `${boxHeight * lengths}px`;
+
+wrapper.style.bottom = `${sliderBottom}px`;
+
+
+function update(start){
+	sliderBottom = start * boxHeight * - 1;
+	wrapper.style.bottom = `${sliderBottom}px`;
+	console.log(start);
+}
+
+
+buttonTop.addEventListener("click", function(e){
+	start = start - 1 < 0 ? lengths - 1  : start - 1;
+	update(start);  
+})
+
+
+
+
 
 /*import $ from 'jquery';*/
 
